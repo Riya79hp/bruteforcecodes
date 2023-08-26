4ed07060dbeb9fa72d01ea104d68a7432e8114f5
Riya79hp <139640191+Riya79hp@users.noreply.github.com>
2023-08-26 18:53:39 +0530

    Createfindiftreeisbalancedornot.c

---
+++ find if tree is balanced or not.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node{
+	int data;
+	struct Node *left;
+	struct Node *right;
+};
+struct Node * create(int data){
+	struct Node *new_p;
+	new_p=(struct Node *)malloc(sizeof(struct Node));
+	new_p->data=data;
+	new_p->left=NULL;
+	new_p->right=NULL;
+	return new_p;
+}
+int height(struct Node *root){
+    if(root==NULL){
+        return 0;
+    }
+    int r=height(root->left);
+    int l=height(root->right);
+      int h;
+    if(r>l){
+       h=r+1;
+    }
+    else{
+       h=l+1;
+    }
+    return h;
+}
+
+
+
+
+int main(){
+
+struct Node *head;
+	head=(struct Node *)malloc(sizeof(struct Node));
+	head->data=56;
+	head->left=create(3);
+	head->left->left=create(77);
+
+
+    	if(height(head->left)-height(head->right)>=1 || height(head->left)-height(head->right)<=1){
+		printf("unbalanced");
+	}
+	else{
+		printf("balanced");
+	}
+
+    
+}
+	
