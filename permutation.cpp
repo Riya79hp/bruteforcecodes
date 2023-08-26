5e2aa3e890d10d180e4dd4e4330752824ba40b68
Riya79hp <139640191+Riya79hp@users.noreply.github.com>
2023-08-26 21:42:28 +0530

    Create permutation.cpp
    this code calculate permutation with recursive approach and uses fi index and swapping it with every index

---
+++ permutation.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+void swap(int &a,int &b){
+	int temp=a;
+	a=b;
+	b=temp;
+}
+void permutation(int arr[],int n,int fi){
+	int i=0;
+	if(fi==n){
+		for(i=0;i<n;i++){
+			cout<<arr[i]<<" ";
+			
+		}
+		cout<<endl;
+	}
+	for(i=fi;i<n;i++){
+		swap(arr[i],arr[fi]);
+		permutation(arr,n,fi+1);
+		swap(arr[i],arr[fi]);
+	
+	}
+}
+
+
+int main(){
+	int n;
+	cin>>n;
+	int arr[50];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+ permutation(arr,n,0);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
